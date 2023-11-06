1abf1b698e98801e1ca25399f12e4c3dc2475e41
FrankSalazar71 <140365851+FrankSalazar71@users.noreply.github.com>
2023-11-06 17:54:37 -0500

    conexion sql

---
+++ Nodejs_sql_express/index.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+
+const conex = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    database: 'sakila'
+});
+
+conex.connect((err)=>{
+    if(err){
+        throw err;
+    }else{
+        console.log('conexion realizado con exitosa');
+    }
+});
+
+conex.query('SELECT  * FROM actor', function(err,result, fields){
+    if(err) throw err;
+
+    result.forEach(result => {
+        console.log(result);
+        
+    });
+})
+conex.end();
