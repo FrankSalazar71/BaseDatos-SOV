db4351a30f5bc5120bea5e6b59c5a788b08596f9
FrankSalazar71 <140365851+FrankSalazar71@users.noreply.github.com>
2023-12-05 16:37:50 -0500

    configuracion y conexion del formulario con la base de datos

--- formularioRegistro/index.js
+++ formularioRegistro/index.js
@@ -42,27 +42,27 @@ app.get("/",function(req,res){
 app.post("/user-form", function(req, res) {
     //Hemos usado el let solo para id y el datos.### para name
   const datos = req.body;
-  let carre = datos.carrera;
-  let nombresComple = datos.nombresCompletos;
+  let carre = datos.especialidad;
+  let nombresComple = datos.nombre;
   let Ndni = datos.dni;
-  let fechaNaci = datos.fechaNacimiento;
-  let correo = datos.correo;
+  let fechaNaci = datos.edad;
+  let correo = datos.correo_electronico;
   let contra = datos.contraseña;
   
   //Hemos usado el let en la sintaxi de mysql 
-  let registrar = "INSERT INTO RECLAMOS carrera, (nombresCompletos, dni, fechaNacimiento, correo, contraseña) VALUES ('" + carre + "','" + nombresComple + "','" + Ndni + "','" + fechaNaci + "','" + correo + "','" + contra + "')";
+  let registrar = "INSERT INTO RECLAMOS (carrera,nombresCompletos, dni, fechaNacimiento, correo, contraseña) VALUES ('" + carre + "','" + nombresComple + "','" + Ndni + "','" + fechaNaci + "','" + correo + "','" + contra + "')";
 
   conexion.query(registrar, function(error) {
     if (error) {
       throw error;
     } else {
       console.log(
-        "CARRERA: " + datos.carre + "\n" + 
-        "NOMBRESCOMPLETO: " + datos.nombresComple + "\n" + 
-        "NUMERODNI: " + datos.Ndni + "\n" +
-        "FNACIMIENTO: " + datos.fechaNaci + "\n" + 
-        "CORREO: " + datos.correo + "\n" + 
-        "CONTRA: " +datos.contra + "\n" 
+        "CARRERA: " + datos.especialidad + "\n" + 
+        "NOMBRESCOMPLETO: " + datos.nombre + "\n" + 
+        "NUMERODNI: " + datos.dni + "\n" +
+        "FNACIMIENTO: " + datos.edad + "\n" + 
+        "CORREO: " + datos.correo_electronico + "\n" + 
+        "CONTRA: " +datos.contraseña + "\n" 
       )
     }
   });
